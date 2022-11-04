c156013c029cb988446480df6772719780756534
monezero <monezerolindo@hotmail.com>
2022-11-04 16:44:52 -0300

    g

---
+++ src/screens/Signin.tsx
@@ -0,0 +1,18 @@
+import { Center, Text, Icon } from "native-base";
+import { Fontisto } from "@expo/vector-icons";
+
+import Logo from "../assets/logo.svg";
+import { Button } from "../components/Button";
+
+export function SignIn() {
+  return (
+    <Center flex={1} bgColor="gray.900">
+      <Logo width={212} height={40} />
+
+      <Button
+        title="ENTRAR COM GOOGLE"
+        leftIcon={<Icon as={Fontisto} name="google" color="white" size="md" />}
+      />
+    </Center>
+  );
+}
